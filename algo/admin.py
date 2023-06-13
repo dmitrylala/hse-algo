9e1bb0c72fa0424bd3382df333850aa5036b25dc
yulachi <yuafilippova@edu.hse.ru>
2023-06-13 18:40:53 +0300

    refactor admin using decorator

--- algo/admin.py
+++ algo/admin.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 from .models import Task
 
 
+@admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ("a", "b", "c", "timestamp")
     search_fields = ["a", "b", "c"]
-
-
-admin.site.register(Task, TaskAdmin)
